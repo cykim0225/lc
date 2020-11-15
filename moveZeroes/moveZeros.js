906dbf98e2c14af22048c0fa530bf66a34c2a6e6
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-11-14 20:26:23 -0800

    Completed Move Zeroes

---
+++ moveZeroes/moveZeros.js
@@ -0,0 +1,11 @@
+var moveZeroes = function(nums) {
+  let lastNonZero = 0;
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+          nums[lastNonZero++] = nums[i];
+      };
+  }
+  for (let j = lastNonZero; j < nums.length; j++) {
+      nums[j] = 0;
+  }
+};
