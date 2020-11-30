8f8706d6ce3026ce39211eb2930d46a227d0f6db
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-11-30 00:40:17 -0800

    Completed House Robber I with recursion + memo

--- houseRobber/houseRobberI.js
+++ houseRobber/houseRobberI.js
@@ -11,3 +11,25 @@ var rob = function (nums) {
   return recurse(nums, nums.length - 1);
 }
 
+//Recursion + memo
+
+var rob = function (nums) {
+  let memo = new Array(nums.length + 1);
+  memo.fill(-1);
+
+  let recursion = (nums, i) => {
+    if (i < 0) {
+      return 0;
+    }
+    if (memo[i] >= 0) {
+      return memo[i];
+    }
+
+    let result = Math.max(recurse(nums, i - 2) + nums[i], recurse(nums, i - 1));
+    memo[i] = result;
+
+    return result;
+  }
+
+  return recurse(nums, nums.length - 1);
+}
