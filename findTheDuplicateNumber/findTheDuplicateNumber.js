bba74b920431ae9b89674d5e91fe6711978196d0
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-12-02 00:43:36 -0800

    Completed Find the Duplicate Number with Floyd Algorithm

--- findTheDuplicateNumber/findTheDuplicateNumber.js
+++ findTheDuplicateNumber/findTheDuplicateNumber.js
@@ -3,15 +3,33 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-  let obj = {};
+  // let obj = {};
 
-  for (let i = 0; i < nums.length; i++) {
-      !obj[nums[i]] ? obj[nums[i]] = 1 : obj[nums[i]]++;
+  // for (let i = 0; i < nums.length; i++) {
+  //     !obj[nums[i]] ? obj[nums[i]] = 1 : obj[nums[i]]++;
+  // }
+
+  // for (let key in obj) {
+  //     if (obj[key] > 1) {
+  //         return key;
+  //     }
+  // }
+
+  // Floyd Algorithm
+  let slow = nums[0];
+  let fast = nums[nums[0]];
+
+  while (fast !== slow) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
   }
 
-  for (let key in obj) {
-      if (obj[key] > 1) {
-          return key;
-      }
+  fast = 0;
+
+  while (fast !== slow) {
+    slow = nums[slow];
+    fast = nums[fast];
   }
+
+  return slow;
 };
