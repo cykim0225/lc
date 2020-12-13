d0429ecc845232115d893bbcdec819941b5ec176
Yeongho (Chris) Kim <yck0225@gmail.com>
2020-12-13 01:09:34 -0800

    Completed Kth Smallest Element In A Sorted Matrix with Binary Search

--- kthSmallestElementInASortedMatrix/kthSmallestElementInASortedMatrix.js
+++ kthSmallestElementInASortedMatrix/kthSmallestElementInASortedMatrix.js
@@ -8,3 +8,32 @@ var kthSmallest = function(matrix, k) {
   arr.sort((a, b) => a - b);
   return arr[k - 1]
 };
+
+//Binary search
+var kthSmallest = function(matrix, k) {
+  let n = matrix.length;
+  let low = matrix[0][0];
+  let high = matrix[n - 1][n - 1];
+
+  while (low < high) {
+      let mid = Math.floor((high - low) / 2) + low;
+      let count = 0;
+
+      for (let i = 0; i < n; i++) {
+          for (let j = 0; j < n; j++) {
+              if (matrix[i][j] <= mid) {
+                  count++;
+              } else {
+                  break;
+              }
+          }
+      }
+      if (count < k) {
+          low = mid + 1;
+      } else {
+          high = mid;
+      }
+  }
+
+  return low;
+};
