ffd26c7bdc08ec427ff128653354dfb498c6370d
Yeongho (Chris) Kim <yck0225@gmail.com>
2021-01-24 21:49:10 -0800

    Completed Longest Increasing Subsequence with dp

--- longestIncreasingSubsequence/longestIncresingSubsequence.js
+++ longestIncreasingSubsequence/longestIncresingSubsequence.js
@@ -19,3 +19,29 @@ var lengthOfLIS = function(nums) {
 
   return recursion(nums, -Infinity, 0);
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  if (nums.length === 0) {
+      return 0;
+  }
+
+  let dp = new Array(nums.length);
+  dp[0] = 1;
+
+  let maxAns = 1;
+  for (let i = 1; i < nums.length; i++) {
+      let maxVal = 0;
+      for (let j = 0; j < i; j++) {
+          if (nums[i] > nums[j]) {
+              maxVal = Math.max(maxVal, dp[j]);
+          }
+          dp[i] = maxVal + 1;
+          maxAns = Math.max(maxAns, dp[i]);
+      }
+  }
+  return maxAns;
+};
